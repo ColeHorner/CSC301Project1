66db9c848c4053bf45d6fc2f811a791a45cb3a4b
Cole Horner <colehorner94@gmail.com>
2018-04-18 00:30:17 -0500

    Skeleton
    Added skeleton methods and class for project.

---
+++ Percolation.java
@@ -0,0 +1,51 @@
+public class Percolation {
+
+  private int openSites;
+
+  /*create N-by-N grid, with all sites initially blocked, also stuff*/
+  public Percolation(int N)
+  {
+
+  }
+
+  /*Opens a site, setting isOpen for this site to be true.*/
+  public void open(int row, int col)
+  {
+
+  }
+
+  /*Returns true if the site is opened (able to be filled) or false otherwise.*/
+  public boolean isOpen(int row, int col)
+  {
+
+  return false; //return placeholder
+  }
+
+  /*Returns true if the site is full (connected to the simulated top site) or *
+   *false otherwise.                                                          */
+  public boolean isFull(int row, int col)
+  {
+
+  return false; //return placeholder
+  }
+
+  /*Returns the number of opensites in the grid.*/
+  public int numberOfOpenSites()
+  {
+
+  return openSites; //counter should be accurate if updated correctly.
+  }
+
+  /*Returns true if the system percolates or false otherwise.*/
+  public boolean percolates()
+  {
+
+  return false; //return placeholder
+  }
+
+  /*Stuff*/
+  public static void main(String[] args)
+  {
+       
+  }
+}
